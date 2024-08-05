9ef8b3f2ee78fe3787533b29209b7ceaca7f6342
yosifov <ivan.yosifov88@gmail.com>
2024-08-05 19:33:42 +0300

    new func added

--- utils.py
+++ utils.py
@@ -5,8 +5,8 @@ def last_function():
     return 43       # This is the line we want to test
 
 
-def some_other_function():
-    return 42   # This is the line we want to test  
+def some_other_new_func():
+    return 44
 
 def another_function():
     return 43       # This is the line we want to test  
