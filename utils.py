a3baaf8221de5cb1e38efa4796ad39eeb722c9eb
yosifov <ivan.yosifov88@gmail.com>
2024-08-05 19:13:50 +0300

    new third func

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 def some_function():
     return 42   # This is the line we want to test
 
+def some_other_function():
+    return 42   # This is the line we want to test  
+
 def another_function():
     return 43       # This is the line we want to test  
